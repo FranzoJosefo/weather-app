ece564d0a9a6b318398e1001dcb2e8b7c19d24f6
Francisco Olivero <franj.olivero@gmail.com>
2020-04-07 20:05:07 -0300

    Added WeatherActivity and setup gradle dependencies to be used

---
+++ app/src/main/java/com/franciscoolivero/android/weatherapp/view/WeatherActivity.java
@@ -0,0 +1,16 @@
+package com.franciscoolivero.android.weatherapp.view;
+
+import android.os.Bundle;
+
+import com.franciscoolivero.android.weatherapp.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ForecastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forecast);
+    }
+}
