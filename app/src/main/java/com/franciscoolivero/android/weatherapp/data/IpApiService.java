302c5d95a58941404cfc1cfcda6da548f848d8c5
Francisco Olivero <franj.olivero@gmail.com>
2020-04-09 10:25:06 -0300

    Added correct endpoint for location GET

--- app/src/main/java/com/franciscoolivero/android/weatherapp/data/IpApiService.java
+++ app/src/main/java/com/franciscoolivero/android/weatherapp/data/IpApiService.java
@@ -1,7 +1,13 @@
 package com.franciscoolivero.android.weatherapp.data;
 
+import com.franciscoolivero.android.weatherapp.model.LocationModel;
+
+import io.reactivex.Single;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public interface IpApiService {
 
-//    @GET("json/{ip_address}")
-//    Single<String> getWeatherData(@Query(value = "lat", encoded = true) String lat, @Query(value = "lon", encoded = true) String lon);
+    @GET("json/{ip_address}")
+    Single<LocationModel> getCurrentLocation(@Path(value = "ip_address", encoded = true) String ip_address);
 }
