1dfde803d3cb154fa65820216fc5f314144ea5e6
Francisco Olivero <franj.olivero@gmail.com>
2020-04-09 10:25:39 -0300

    Created Location model for IPAPI response

---
+++ app/src/main/java/com/franciscoolivero/android/weatherapp/model/LocationModel.java
@@ -0,0 +1,51 @@
+package com.franciscoolivero.android.weatherapp.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class LocationModel {
+    @SerializedName("status")
+    private String responseStatus;
+    @SerializedName("lat")
+    private Double currentLatitude;
+    @SerializedName("lon")
+    private Double currentLongitude;
+    @SerializedName("country")
+    private String countryName;
+    @SerializedName("regionName")
+    private String regionName;
+    @SerializedName("city")
+    private String cityName;
+
+    public LocationModel(String responseStatus, Double currentLatitude, Double currentLongitude, String countryName, String regionName, String cityName) {
+        this.responseStatus = responseStatus;
+        this.currentLatitude = currentLatitude;
+        this.currentLongitude = currentLongitude;
+        this.countryName = countryName;
+        this.regionName = regionName;
+        this.cityName = cityName;
+    }
+
+    public String getResponseStatus() {
+        return responseStatus;
+    }
+
+    public Double getCurrentLatitude() {
+        return currentLatitude;
+    }
+
+    public Double getCurrentLongitude() {
+        return currentLongitude;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public String getRegionName() {
+        return regionName;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+}
