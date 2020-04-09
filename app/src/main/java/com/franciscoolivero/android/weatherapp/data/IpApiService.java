d7b61c42d2c49dc3a4897f26a05462cc89b4d0c7
Francisco Olivero <franj.olivero@gmail.com>
2020-04-09 18:47:01 -0300

    Remove unneccesary NetUtils class small Refactor IpApiService

--- app/src/main/java/com/franciscoolivero/android/weatherapp/data/IpApiService.java
+++ app/src/main/java/com/franciscoolivero/android/weatherapp/data/IpApiService.java
@@ -4,10 +4,9 @@ import com.franciscoolivero.android.weatherapp.model.LocationModel;
 
 import io.reactivex.Single;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
 
 public interface IpApiService {
 
-    @GET("json/{ip_address}")
-    Single<LocationModel> getCurrentLocation(@Path(value = "ip_address", encoded = true) String ip_address);
+    @GET("json")
+    Single<LocationModel> getCurrentLocation();
 }
