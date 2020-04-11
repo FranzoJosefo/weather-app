b3b523b91117f96ea4b661aca00de1ea122d6c08
Francisco Olivero <franj.olivero@gmail.com>
2020-04-10 21:44:44 -0300

    Added swipe to refresh layout

--- app/src/main/java/com/franciscoolivero/android/weatherapp/view/WeatherActivity.java
+++ app/src/main/java/com/franciscoolivero/android/weatherapp/view/WeatherActivity.java
@@ -3,6 +3,7 @@ package com.franciscoolivero.android.weatherapp.view;
 import android.os.Bundle;
 
 import com.franciscoolivero.android.weatherapp.R;
+import com.franciscoolivero.android.weatherapp.viewmodel.WeatherViewModel;
 import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
@@ -10,6 +11,8 @@ import com.google.android.material.tabs.TabLayoutMediator;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import androidx.viewpager2.widget.ViewPager2;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -31,15 +34,30 @@ public class WeatherActivity extends AppCompatActivity {
     @BindView(R.id.weather_view_pager)
     ViewPager2 viewPager;
 
+    @BindView(R.id.swipe_refresh_layout)
+    SwipeRefreshLayout swipeRefreshLayout;
+
+    private WeatherViewModel weatherViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
         ButterKnife.bind(this);
+        weatherViewModel = ViewModelProviders.of(this).get(WeatherViewModel.class);
         setupTabLayoutWithViewPager();
+        setupSwipeRefreshListener();
+    }
+
+    private void setupSwipeRefreshListener() {
+        swipeRefreshLayout.setOnRefreshListener(() -> {
+            weatherViewModel.fetchLocationAndWeatherData(true);
+            swipeRefreshLayout.setRefreshing(false);
+        });
     }
 
     private void setupTabLayoutWithViewPager() {
+        viewPager.setUserInputEnabled(false);
         TabsPagerAdapter tabsPagerAdapter = new TabsPagerAdapter(this);
         viewPager.setAdapter(tabsPagerAdapter);
         new TabLayoutMediator(tabLayout, viewPager,
